ede9849c06bd286344fbda195221d37c0576c518
andy173417 <41251070+andy173417@users.noreply.github.com>
2018-10-23 13:26:13 +0800

    Update 28. Implement strStr().cpp

--- Easy/28. Implement strStr().cpp
+++ Easy/28. Implement strStr().cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        return haystack.find(needle);
+        if(needle.size()==0)
+            return 0;
+        else if(haystack.find(needle)==string::npos)
+            return -1;
+        else
+            return haystack.find(needle);
     }
 };
