4475b16cee8cce47ef851d43be4ee23f321852d2
andy173417 <41251070+andy173417@users.noreply.github.com>
2018-11-14 00:09:25 +0800

    Create 58. Length of Last Word.cpp

---
+++ Easy/58. Length of Last Word.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count=0,last=s.size();
+        while(s[last-1]==' ') last--;
+        for(int i=0;i<last;i++){
+            if(s[i]!=' '){
+                count++;
+            }
+            else{
+                count=0;
+            }
+        }
+        return count;
+    }
+};
+
+
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count=0,last=s.size();
+        if (s.length() == 0) {
+            return 0;
+        }
+        while(s[last-1]==' ') last--;
+        for(int i=last-1;i>=0;i--){
+            if(s[i]!=' '){
+                count++;
+            }
+            else{
+                break;
+            }
+        }
+        return count;
+    }
+};
